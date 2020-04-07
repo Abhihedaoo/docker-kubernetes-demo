171ffe6deb20637c9e93d908c4c277ecd6c8c72d
Abhishek Hedaoo <abhihedaoo98@gmail.com>
2020-04-07 08:19:37 +0000

    Adde the prometheus and grafana kubernetes depoloyment and monitoring th flaks app through it

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
 from typing import List, Dict
 from flask import Flask
+from prometheus_client import Summary, Gauge, Counter, generate_latest, REGISTRY, Histogram
+import random
 import mysql.connector
 import json
 
 app = Flask(__name__)
+PYTHON_RANDOM_VALUE = Gauge('python_random_value', 'randomly generated in python')
 
 
 def favorite_colors() -> List[Dict]:
@@ -27,6 +30,18 @@ def favorite_colors() -> List[Dict]:
 @app.route('/')
 def index() -> str:
     return json.dumps({'favorite_colors': favorite_colors()})
+
+
+@app.route('/random')
+def randomPage():
+    PYTHON_RANDOM_VALUE.set(random.random())
+    return ("Random Page")
+
+
+@app.route('/metrics', methods=['GET'])
+def stats():
+    return generate_latest(REGISTRY), 200
+
 
 
 if __name__ == '__main__':
